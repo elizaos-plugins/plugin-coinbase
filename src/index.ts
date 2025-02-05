bf5c1e080c98f14b6e5ff789a044e2fc9f5964d4
Avaer Kazmer <ultimus@gmail.com>
2025-02-04 23:46:02 -0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,9 @@ export const plugins = {
     advancedTradePlugin,
 };
 
-function mergePlugins(base: object, plugins: any[]) {
-    return {
-        ...base,
-        actions: [...plugins.map(plugin => plugin.actions)],
-        providers: [...plugins.map(plugin => plugin.providers)],
-        evaluators: [...plugins.map(plugin => plugin.evaluators)],
-        services: [...plugins.map(plugin => plugin.services)],
-    };
-}
-const mergedPlugins = mergePlugins({
-    name: 'coinbase',
-    description: 'Coinbase plugin. Enables various functionalities using the Coinbase SDK.',
-}, Object.values(plugins));
-export default mergedPlugins;
+export * from "./plugins/massPayments";
+export * from "./plugins/commerce";
+export * from "./plugins/trade";
+export * from "./plugins/tokenContract";
+export * from "./plugins/webhooks";
+export * from "./plugins/advancedTrade";
