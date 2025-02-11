3fb49a71cf884c7b31f555834f1d494671c99cfd
Carlos V <carlosv.provoos@gmail.com>
2025-02-10 22:31:12 -0500

    last build of the repo

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,10 @@
-import * as _elizaos_core from '@elizaos/core';
-
 declare const plugins: {
-    coinbaseMassPaymentsPlugin: _elizaos_core.Plugin;
-    coinbaseCommercePlugin: _elizaos_core.Plugin;
-    tradePlugin: _elizaos_core.Plugin;
-    tokenContractPlugin: _elizaos_core.Plugin;
-    webhookPlugin: _elizaos_core.Plugin;
-    advancedTradePlugin: _elizaos_core.Plugin;
+    coinbaseMassPaymentsPlugin: Plugin;
+    coinbaseCommercePlugin: Plugin;
+    tradePlugin: Plugin;
+    tokenContractPlugin: Plugin;
+    webhookPlugin: Plugin;
+    advancedTradePlugin: Plugin;
 };
 
 export { plugins };
